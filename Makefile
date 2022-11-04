4f4cd865c6684db914ba8dd3250b16fac6b7c223
Tomoya Fujiwara <tomofuji.dev@gmail.com>
2022-11-04 15:07:55 +0900

    modify makefile

--- Makefile
+++ Makefile
@@ -9,9 +9,9 @@ LIBFT		=	$(LIBFT_DIR)/libft.a
 MLX_DIR		=	minilibx
 MLX			=	$(MLX_DIR)/libmlx.a
 
-LIB_DIR		=	-L $(LIBFT_DIR) -L $(MLX_DIR) -L/usr/X11R6/lib
+LIB_DIR		=	-L $(LIBFT_DIR) -L $(MLX_DIR) -L /usr/X11/lib
 INCLUDE_DIR	=	include
-INCLUDES	=	-I $(INCLUDE_DIR)
+INCLUDES	=	-I $(INCLUDE_DIR) -I $(MLX_DIR) -I /usr/X11/include
 SRCDIRS		=	$(shell find $(SRC_DIR) -type d)
 INCLUDES 	+=	$(addprefix -I,$(SRCDIRS))
 
@@ -35,13 +35,13 @@ endif
 
 # macos x86
 ifeq ($(UNAME_S),Darwin)
-	LIBRARY = -lmlx
+	LIBRARY = -lmlx -lXext -lX11
 	FRAMEWORK = -framework OpenGL -framework AppKit
 endif
 
 # macos ARM (m1/m2...)
 ifneq ($(filter arm%, $(UNAME_P)),)
-	LIBRARY = -lmlx
+	LIBRARY = -lmlx -lXext -lX11
 	FRAMEWORK = -framework OpenGL -framework AppKit
 endif
 
--- src/test.c
+++ src/test.c
@@ -6,13 +6,18 @@
 /*   By: tfujiwar <tfujiwar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 11:07:56 by tfujiwar          #+#    #+#             */
-/*   Updated: 2022/11/04 11:20:01 by tfujiwar         ###   ########.fr       */
+/*   Updated: 2022/11/04 14:39:46 by tfujiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include "mlx.h"
 
 int	main(void)
 {
-	printf("hi\n");
+	void	*mlx_ptr;
+	void	*win_ptr;
+
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 1000, 500, "test");
 }
