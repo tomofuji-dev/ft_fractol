55a9a1c497958e58fe8a50981c1f551fd254816c
Tomoya Fujiwara <tomofuji.dev@gmail.com>
2022-11-10 10:10:06 +0900

    encode_rgb

--- src/draw/util.c
+++ src/draw/util.c
@@ -6,7 +6,7 @@
 /*   By: tfujiwar <tfujiwar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 19:59:15 by tfujiwar          #+#    #+#             */
-/*   Updated: 2022/11/08 20:16:16 by tfujiwar         ###   ########.fr       */
+/*   Updated: 2022/11/10 10:09:52 by tfujiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,9 @@ void	pixel_put(t_env *env, int x, int y, int color)
 
 	dst = env->img_data + (y * env->bytes_per_line + x * env->bytes_per_pixel);
 	*(int *)dst = color;
+}
+
+int	encode_rgb(uint8_t red, uint8_t green, uint8_t blue)
+{
+	return (red << 16 | green << 8 | blue);
 }
